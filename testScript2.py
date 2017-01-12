4b8f5bbe5c4f2da57e0e05b86c946f8998e89994
Filip Szpila <filip.szpila@soflab.pl>
2017-01-12 18:50:51 +0100

    initial commit

---
+++ testScript2.py
@@ -0,0 +1,23 @@
+import unittest
+from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+
+class CRUD(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome('.\chromedriver.exe')
+
+    def test_create(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+
+    def test_read(self):
+        driver = self.driver
+        driver.get("http://www.google.com")
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
