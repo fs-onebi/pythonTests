0beaccf26da09c8a38e540d22285560219adef6e
Bartlomiej Szewczyk <bartlomiej.szewczyk@onebi.eu>
2017-01-13 12:20:23 +0100

    Added test: test_adding_computer_validation

--- features/testScriptCreate.py
+++ features/testScriptCreate.py
@@ -54,6 +54,49 @@ class Create(unittest.TestCase):
 
         self.assertTrue(listContainsValue, "List of computers not contain expected value: " + computerName + ".")
 
+    def test_adding_computer_validation(self):
+        driver = self.driver
+        driver.get("http://computer-database.herokuapp.com/computers")
+
+        introducedDate = "20160810"
+        discontinuedDate = "20170105"
+
+        driver.find_element_by_id('add').click()
+
+        header = driver.find_element_by_xpath('//*[@id="main"]/h1')
+        self.assertEqual(header.text, "Add a computer", "Text from success message not equals text: Add a computer.")
+
+        validationTextComputerName = driver.find_element_by_xpath('//*[@id="name"]/following-sibling::span')
+        self.assertEqual(validationTextComputerName.text, "Required",
+                         "Text from success message not equals text: Required.")
+
+        validationTextIntroducedDate = driver.find_element_by_xpath('//*[@id="introduced"]/following-sibling::span')
+        self.assertEqual(validationTextIntroducedDate.text, "Date ('yyyy-MM-dd')",
+                         "Text from success message not equals text: Date ('yyyy-MM-dd').")
+
+        validationTextDiscontinuedDate = driver.find_element_by_xpath('//*[@id="discontinued"]/following-sibling::span')
+        self.assertEqual(validationTextDiscontinuedDate.text, "Date ('yyyy-MM-dd')",
+                         "Text from success message not equals text: Date ('yyyy-MM-dd').")
+
+        driver.find_element_by_id('introduced').send_keys(introducedDate)
+        driver.find_element_by_id('discontinued').send_keys(discontinuedDate)
+        driver.find_element_by_xpath("//div[@class='actions']/input").click()
+
+        validationComputerName = driver.find_element_by_xpath("//*[@for='name']/parent::div")
+        self.assertIn("error", validationComputerName.get_attribute('class'),
+                      "Field " + validationComputerName.find_element_by_xpath("..//*[@for='name']").text
+                      + " doesn't show validation")
+
+        validationIntroducedDate = driver.find_element_by_xpath("//*[@for='introduced']/parent::div")
+        self.assertIn("error", validationIntroducedDate.get_attribute('class'),
+                      "Field " + validationIntroducedDate.find_element_by_xpath("..//*[@for='introduced']").text
+                      + " doesn't show validation")
+
+        validationDiscontinuedDate = driver.find_element_by_xpath("//*[@for='discontinued']/parent::div")
+        self.assertIn("error", validationDiscontinuedDate.get_attribute('class'),
+                      "Field " + validationDiscontinuedDate.find_element_by_xpath("..//*[@for='discontinued']").text
+                      + " doesn't show validation")
+
     def tearDown(self):
         self.driver.close()
 
