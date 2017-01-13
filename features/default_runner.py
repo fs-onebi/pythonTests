1e22f84782bb93f320b55e3fca2d0f0b8e140e00
damian.kwasny <damian.kwasny@soflab.pl>
2017-01-13 12:52:23 +0100

    reports

--- features/default_runner.py
+++ features/default_runner.py
@@ -3,6 +3,7 @@ import testScriptDelete
 import testScriptRead
 import testScriptCreate
 import testScriptUpdate
+from lib import HTMLTestRunner
 
 class Test_Suite(unittest.TestCase):
     def test_main(self):
@@ -11,10 +12,18 @@ class Test_Suite(unittest.TestCase):
         self.suite.addTests([
             unittest.defaultTestLoader.loadTestsFromTestCase(testScriptCreate.Create),
             unittest.defaultTestLoader.loadTestsFromTestCase(testScriptRead.Read),
-            unittest.defaultTestLoader.loadTestsFromTestCase(testScriptUpdate.Update),
-            unittest.defaultTestLoader.loadTestsFromTestCase(testScriptDelete.Delete)
+            # unittest.defaultTestLoader.loadTestsFromTestCase(testScriptUpdate.Update),
+            # unittest.defaultTestLoader.loadTestsFromTestCase(testScriptDelete.Delete)
         ])
-        runner = unittest.TextTestRunner()
+        # runner = unittest.TextTestRunner()
+        runner = unittest.TextTestRunner(verbosity=1)
+        filename = "../reports/default_runner_report.html"
+        output = open(filename, "wb")
+        runner = HTMLTestRunner.HTMLTestRunner(
+            stream=output,
+            title="Test report"
+        )
+
         runner.run(self.suite)
 
 if __name__ == "__main__":
